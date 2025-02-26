9e342246a7b89ec702f894ddf6a42611325d19bf
David <davidgimenez_cs@hotmail.com>
2025-02-26 19:16:49 +0100

    WebConfig.java

---
+++ src/main/java/org/edgar/hodlverse/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.edgar.hodlverse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
